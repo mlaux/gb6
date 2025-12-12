c3ff637f6ce00f70a4dc4fceff9a472553ecf542
Matthew Laux <matthew.laux@gmail.com>
2025-12-12 17:47:36 -0600

    simplify how returning from the test works

--- compiler/tests/test_compiler.c
+++ compiler/tests/test_compiler.c
@@ -6,14 +6,10 @@
 #include "../compiler.h"
 #include "../musashi/m68k.h"
 
-// Simple memory for the 68k emulator
 #define MEM_SIZE 0x10000
 static uint8_t mem[MEM_SIZE];
 
-// Code gets loaded here
 #define CODE_BASE 0x1000
-
-// Stack starts here (grows down)
 #define STACK_BASE 0x8000
 
 // Memory access callbacks for Musashi
@@ -22,7 +18,8 @@ unsigned int m68k_read_memory_8(unsigned int address)
     if (address < MEM_SIZE) {
         return mem[address];
     }
-    return 0;
+    printf("attempted to read byte from %08x\n", address);
+    exit(1);
 }
 
 unsigned int m68k_read_memory_16(unsigned int address)
@@ -30,7 +27,8 @@ unsigned int m68k_read_memory_16(unsigned int address)
     if (address + 1 < MEM_SIZE) {
         return (mem[address] << 8) | mem[address + 1];
     }
-    return 0;
+    printf("attempted to read word from %08x\n", address);
+    exit(1);
 }
 
 unsigned int m68k_read_memory_32(unsigned int address)
@@ -39,13 +37,16 @@ unsigned int m68k_read_memory_32(unsigned int address)
         return (mem[address] << 24) | (mem[address + 1] << 16) |
                (mem[address + 2] << 8) | mem[address + 3];
     }
-    return 0;
+    printf("attempted to read long from %08x\n", address);
+    exit(1);
 }
 
 void m68k_write_memory_8(unsigned int address, unsigned int value)
 {
     if (address < MEM_SIZE) {
         mem[address] = value & 0xff;
+    } else {
+        printf("attempted to write byte %08x = %d\n", address, value);
     }
 }
 
@@ -54,6 +55,8 @@ void m68k_write_memory_16(unsigned int address, unsigned int value)
     if (address + 1 < MEM_SIZE) {
         mem[address] = (value >> 8) & 0xff;
         mem[address + 1] = value & 0xff;
+    } else {
+        printf("attempted to write word %08x = %d\n", address, value);
     }
 }
 
@@ -64,10 +67,11 @@ void m68k_write_memory_32(unsigned int address, unsigned int value)
         mem[address + 1] = (value >> 16) & 0xff;
         mem[address + 2] = (value >> 8) & 0xff;
         mem[address + 3] = value & 0xff;
+    } else {
+        printf("attempted to write long %08x = %d\n", address, value);
     }
 }
 
-// Test counters
 static int tests_run = 0;
 static int tests_passed = 0;
 
@@ -110,23 +114,16 @@ static uint32_t run_code(struct basic_block *block)
     // Copy code to CODE_BASE
     memcpy(mem + CODE_BASE, block->code, block->length);
 
-    // Set up reset vector (not really needed since we set PC directly)
-    // Initial SP at 0x0000, Initial PC at 0x0004
-    m68k_write_memory_32(0, STACK_BASE);
-    m68k_write_memory_32(4, CODE_BASE);
+    // each test case will return to address 0, which contains an infinite loop
+    m68k_write_memory_32(STACK_BASE - 4, 0);
 
-    // Push a return address onto the stack so rts has somewhere to go
-    // We'll use address 0 as a sentinel (contains illegal instruction)
-    m68k_write_memory_16(0, 0x4afc);  // illegal instruction at 0
-    m68k_write_memory_32(STACK_BASE - 4, 0);  // return address = 0
+    m68k_pulse_reset();
 
     m68k_set_reg(M68K_REG_SP, STACK_BASE - 4);
+    m68k_set_reg(M68K_REG_ISP, STACK_BASE);
     m68k_set_reg(M68K_REG_PC, CODE_BASE);
-
-    // Clear D0
     m68k_set_reg(M68K_REG_D0, 0);
 
-    // Run until we hit the illegal instruction or enough cycles pass
     int cycles = m68k_execute(1000);
     (void)cycles;
 
